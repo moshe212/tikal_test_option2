2e06a2ba5cb67f666e06d1a1cd8aef9d0d209e73
moshe212 <moshe212@gmail.com>
2021-09-09 22:34:00 +0300

    fix chart

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -8,7 +8,7 @@ const Chart = (props) => {
 
   useEffect(() => {
     if (props.data.length > 0) {
-      console.log("f");
+      console.log(props.data);
       setLoading(false);
     }
   }, [props]);
@@ -18,27 +18,12 @@ const Chart = (props) => {
       <div className="Chart">
         <section className="bar-graph bar-graph-vertical bar-graph-two">
           <h2>Chart</h2>
-
-          <div className="bar-one bar-container">
-            <div className="bar" data-populate={data[0].populate}></div>
-            <span className="planet">Tatooine</span>
-          </div>
-          <div className="bar-two bar-container">
-            <div className="bar" data-populate={data[1].populate}></div>
-            <span className="planet">Alderaan</span>
-          </div>
-          <div className="bar-three bar-container">
-            <div className="bar" data-populate={data[2].populate}></div>
-            <span className="planet">Naboo</span>
-          </div>
-          <div className="bar-four bar-container">
-            <div className="bar" data-populate={data[3].populate}></div>
-            <span className="planet">Bespin</span>
-          </div>
-          <div className="bar-five bar-container">
-            <div className="bar" data-populate={data[4].populate}></div>
-            <span className="planet">Endor</span>
-          </div>
+          {data.map((item, itemIndex) => (
+            <div className={`bar-${itemIndex} bar-container`} key={itemIndex}>
+              <div className="bar" data-populate={item.populate}></div>
+              <span className="planet">{item.name}</span>
+            </div>
+          ))}
         </section>
       </div>
     );
